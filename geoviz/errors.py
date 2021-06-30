9f56dfb8c4babdf9465e9b8b5a88caa172acb3f2
Tony Zeidan <tony.azp25@gmail.com>
2021-06-30 12:50:02 -0400

    User input changes for the builder. More clear definitions made.

--- geoviz/errors.py
+++ geoviz/errors.py
@@ -16,3 +16,39 @@ class BuilderDatasetInfoError(Exception):
     def __init__(self, message="Ivalid information was given with a dataset."):
         self.message = message
         super().__init__(self.message)
+
+class BuilderAlterationError(Exception):
+
+    def __init__(self, message="There was an error while altering data within the builder."):
+        self.message = message
+        super().__init__(self.message)
+
+class ColorscaleError(Exception):
+
+    def __init__(self, message="There was an error while reading the colorscale."):
+        self.message = message
+        super().__init__(self.message)
+
+class DataFileReadError(Exception):
+
+    def __init__(self, message="There was an error while reading the data file."):
+        self.message = message
+        super().__init__(self.message)
+
+class DataReadError(Exception):
+
+    def __init__(self, message="There was an error while reading the data."):
+        self.message = message
+        super().__init__(self.message)
+
+class DataEmptyError(Exception):
+
+    def __init__(self, message="The data can not be empty."):
+        self.message = message
+        super().__init__(self.message)
+
+class BinValueTypeError(Exception):
+
+    def __init__(self, message="The data can not be empty."):
+        self.message = message
+        super().__init__(self.message)
