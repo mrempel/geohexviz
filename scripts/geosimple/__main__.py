6a6b55b6978af8f9debece16310438edad3068c0
Tony Zeidan <tony.azp25@gmail.com>
2021-07-05 15:41:22 -0400

    Fixed script functions and cleaned data.

--- scripts/geosimple/__main__.py
+++ scripts/geosimple/__main__.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any, Callable
 from geoviz.builder import PlotBuilder
+from utils import run_simple_JSON
 import json
 import os
 import sys
@@ -21,7 +22,7 @@ def plot():
                         'your builder parameter file.')
     if not fp:
         return
-    execute_simple_file(fp)
+    run_simple_JSON(fp)
 
 
 def plotDir():
@@ -33,7 +34,7 @@ def plotDir():
 
         try:
             for file in os.listdir(fp):
-                execute_simple_file(os.path.join(fp, file))
+                run_simple_JSON(os.path.join(fp, file))
             break
         except NotADirectoryError:
             print('That was not a directory. Try again or exit.')
@@ -69,82 +70,62 @@ fn_map: Dict[str, Callable] = {
     'simple clip': PlotBuilder.simple_clip,
     'clip datasets': PlotBuilder.clip_datasets,
     'discrete scale': PlotBuilder.discretize_scale,
-    'set mapbox': PlotBuilder.set_mapbox,
-    'output figure': PlotBuilder.output_figure,
-    'display figure': PlotBuilder.display_figure,
-    'clear figure': PlotBuilder.clear_figure,
-    'update main manager': PlotBuilder.update_main_manager,
-    'build plot': PlotBuilder.build_plot
 }
-
-fn_map2 = {' '.join(func.split('_')): getattr(PlotBuilder, func) for func in dir(PlotBuilder) if
-           callable(getattr(PlotBuilder, func)) and not func.startswith('_')}
-print(fn_map2)
 
 output_fns = ['display figure', 'output figure']
 
 
-# TODO: this seems to be the easy option
-# TODO: it limits the users ability to input things in a certain order
-def execute_simple_file(filepath: str):
+def run_simple_JSON(filepath: str):
     with open(filepath) as jse:
         read = json.load(jse)
 
-    default_fns = {
-        'clip datasets': False,
-        'simple clip': False,
-        'generate grid': False,
-        'set mapbox': False,
-        'logarithmic scale': False,
-        'discrete scale': False,
-        'adjust opacity': True,
-        'adjust focus': True,
-        'output figure': False,
-        'display figure': {'renderer': 'browser'}
+    build_args = {
+        "raise_errors": False,
+        "plot_regions": True,
+        "plot_grids": True,
+        "plot_outlines": True,
+        "plot_points": True
     }
 
-    for k, v in read.pop("plot functions", {}).items():
-        if k not in default_fns:
-            raise ValueError(f"Received an invalid function. Valid functions: {list(default_fns.keys())}.")
-        default_fns[k] = v
-
-    opf = default_fns.pop('output figure')
-    dpf = default_fns.pop('display figure')
-    default_fns['build plot'] = {'raise_errors': False}
-    default_fns['output figure'] = opf
-    default_fns['display figure'] = dpf
-
-    builder = PlotBuilder(**read)
-
-    for k, v in default_fns.items():
-        if v != False:
-            args, kwargs = _parse_args_kwargs(v)
-            fn_map2[k](builder, *args, **kwargs)
-
-
-def execute_file(filepath: str):
-    with open(filepath) as jse:
-        read = json.load(jse)
-
-    builder_fns = read.pop('builder_functions', [])
-    if builder_fns:
-        if fkey(builder_fns[-1]) not in output_fns:
-            builder_fns.append({'display figure': True})
-    else:
-        builder_fns.append({'display figure': True})
+    mapbox_fig = read.pop("mapbox_token", False)
+    adjustments = read.pop("adjustments", {})
+    adjust_opacity = adjustments.pop("opacity", True)
+    adjust_colorbar = adjustments.pop("colorbar_size", True)
+    adjust_focus = adjustments.pop("focus", True)
+    build_args.update(adjustments.pop("build", {}))
+    output_fig = read.pop("output_figure", False)
+    display_fig = read.pop("display_figure", True)
+    builder_fns = read.pop("builder_functions", {})
 
     builder = PlotBuilder(**read)
 
-    for k in builder_fns:
-        v = k[fkey(k)]
-        k = fkey(k)
+    for k, v in builder_fns.items():
         if v != False:
             args, kwargs = _parse_args_kwargs(v)
-            try:
-                print(k, args, kwargs)
-                fn_map2[k](builder, *args, **kwargs)
-            except KeyError:
-                raise ValueError(f"There was a function that was not expected. Received {k}.")
+            fn_map[k](builder, *args, **kwargs)
+
+    if mapbox_fig:
+        args, kwargs = _parse_args_kwargs(mapbox_fig)
+        builder.set_mapbox(*args, **kwargs)
+
+    if adjust_focus:
+        args, kwargs = _parse_args_kwargs(adjust_focus)
+        builder.adjust_focus(*args, **kwargs)
+
+    if adjust_opacity:
+        args, kwargs = _parse_args_kwargs(adjust_opacity)
+        print(args, kwargs)
+        builder.adjust_opacity(*args, **kwargs)
+
+    builder.build_plot(**build_args)
+
+    if output_fig:
+        args, kwargs = _parse_args_kwargs(output_fig)
+        builder.output_figure(*args, **kwargs)
+
+    if display_fig:
+        args, kwargs = _parse_args_kwargs(display_fig)
+        builder.display_figure(*args, **kwargs)
 
 
 main_options = {
---
+++ tests/testscripts/test_geosimple.py
@@ -0,0 +1,17 @@
+import unittest
+from os.path import join as pjoin
+import os
+from scripts.geosimple import utils
+from geoviz.builder import PlotStatus
+
+DATA_PATH = pjoin(os.path.dirname(__file__), 'data')
+PARAM_PATH = pjoin(DATA_PATH, 'parameterfile-data')
+
+
+class GeoSimpleCase(unittest.TestCase):
+    def test_run_simple_JSON(self):
+        self.assertEqual(PlotStatus.DATA_PRESENT, utils.run_simple_JSON(pjoin(PARAM_PATH, 'sample1-russia.json')))
+
+
+if __name__ == '__main__':
+    unittest.main()
