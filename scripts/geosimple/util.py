e69e07a59b772a82918d12e5ff9093eb962b6aa3
Tony Zeidan <tony.azp25@gmail.com>
2021-08-20 12:45:14 -0400

    Better Script. Cleaned.

--- scripts/geosimple/util.py
+++ scripts/geosimple/util.py
@@ -15,11 +15,13 @@ fn_map: Dict[str, Callable] = {
 output_fns = ['display figure', 'output figure']
 
 
-def run_json(filepath: str):
+def run_json(filepath: str, debug: bool = False):
     """Runs a json file representation of a plot scheme.
 
     :param filepath: The filepath to the json file
     :type filepath: str
+    :param debug: Determines if parts of the internal processes are printed or not
+    :type debug: bool
     :return: The status of the plot after building
     :rtype: PlotStatus
     """
@@ -34,6 +36,11 @@ def run_json(filepath: str):
         "plot_points": True
     }
 
+    if debug:
+        debugprint = lambda x: print(x)
+    else:
+        debugprint = lambda x: None
+
     mapbox_fig = read.pop("mapbox_token", False)
     adjustments = read.pop("adjustments", {})
     adjust_opacity = adjustments.pop("opacity", True)
@@ -45,23 +52,34 @@ def run_json(filepath: str):
     builder_fns = read.pop("builder_functions", {})
 
     builder = PlotBuilder.builder_from_dict(**read)
+    debugprint("* Datasets loaded")
 
     for k, v in builder_fns.items():
         if v != False:
             args, kwargs = parse_args_kwargs(v)
             fn_map[k](builder, *args, **kwargs)
+            debugprint(f"* Invoked '{fn_map[k].__name__}'")
 
     if mapbox_fig:
         args, kwargs = parse_args_kwargs(mapbox_fig)
         builder.set_mapbox(*args, **kwargs)
+        debugprint("* Converted to MapBox figure")
 
     if adjust_focus:
         args, kwargs = parse_args_kwargs(adjust_focus)
-        builder.adjust_focus(*args, **kwargs)
+        try:
+            builder.adjust_focus(*args, **kwargs)
+            debugprint("* Focus adjusted")
+        except Exception:
+            print("* Error when adjusting focus")
 
     if adjust_opacity:
         args, kwargs = parse_args_kwargs(adjust_opacity)
-        builder.adjust_opacity(*args, **kwargs)
+        try:
+            builder.adjust_opacity(*args, **kwargs)
+            debugprint("* Colorscale opacity adjusted")
+        except Exception:
+            print("* Error when adjusting colorscale opacity")
 
     builder.build_plot(**build_args)
     #builder.adjust_colorbar_size()
@@ -69,9 +87,11 @@ def run_json(filepath: str):
     if output_fig:
         args, kwargs = parse_args_kwargs(output_fig)
         builder.output_figure(*args, **kwargs)
+        debugprint("* Figure output")
 
     if display_fig:
         args, kwargs = parse_args_kwargs(display_fig)
         builder.display_figure(*args, **kwargs)
+        debugprint("* Figure displayed")
 
     return builder.get_plot_status()
