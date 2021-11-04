c0a10c40a490448d96b504cc96b722477e92aa51
tony-zeidan <tony.azp25@gmail.com>
2021-11-04 13:57:39 -0400

    Setup.py versioning and info update.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version="1.0.0",
     description="A library for the visualization of hexagonally binned data sets.",
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="",
     author="Tony Marco Abou Zeidan",
     author_email="tony.azp25@gmail.com",
@@ -17,24 +17,24 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9.4",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)", # ensure license
-        "Operating System :: OS Independent",  # with exceptions TBD
+        "License :: OSI Approved :: BSD License",   # TODO: ensure correct licensing
+        "Operating System :: OS Independent",  # TODO: ensure deployment on other OS
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Scientific/Engineering :: Visualization",
     ],
     keywords='visualization,geospatial,hexagonal binning',
-    python_requires=">=3.1",
+    python_requires=">=3.6",
     packages=find_packages(exclude=tuple("tests")),
     include_package_data=True,
     install_requires=[
-        "h3>=3.7.0",
-        "shapely>=1.7.1",
-        "pyproj>=3.0.0.post1",
-        "numpy>=1.20.3",
-        "geojson>=2.5.0",
-        "pandas>=1.3.0",
-        "plotly>=4.14.3",
-        "kaleido>=0.2.1",
+        "h3~=3.7.0",
+        "shapely~=1.7.1",
+        "pyproj~=3.0.0.post1",
+        "numpy~=1.20.3",
+        "geojson~=2.5.0",
+        "pandas~=1.3.0",
+        "plotly~=4.14.3",
+        "kaleido~=0.2.1",
         "pyyaml~=5.4.1",
         "rtree~=0.9.1"
     ],
@@ -42,9 +42,17 @@ setup(
         "dev": [
             "pip",
             "Sphinx",
-            "openpyxl"
+            "openpyxl~=3.0.9",
+            "pdfcropmargins~=1.0.5"
         ],
         "pdf-crop": [
+            "pdfcropmargins~=1.0.5"
+        ],
+        "excel-enabled": [
+            "openpyxl~=3.0.9"
+        ],
+        "all-extras": [
+            "openpyxl~=3.0.9",
             "pdfcropmargins~=1.0.5"
         ]
     },
