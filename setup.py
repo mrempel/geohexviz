012dac107abc3c1978d45ff473da8ed04942e8ad
Mark Rempel <rempel.mark@gmail.com>
2023-02-04 19:52:05 -0500

    Setup for Python >= 3.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Topic :: Scientific/Engineering"
     ],
     keywords='visualization,geospatial,hexagonal binning',
-    python_requires=">=3.7, <=3.8.16",
+    python_requires=">=3.7",
     packages=find_packages(exclude=["docs", "tests"]),
     include_package_data=True,
     install_requires=[
